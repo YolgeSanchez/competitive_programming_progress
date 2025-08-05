1c9a1667fea569e8d8b0658240fedcff018d99ec
YolgeSanchez <imjorgesanchez@gmail.com>
2025-08-04 23:33:32 -0400

    soldier and bananas 546A

---
+++ 546A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+void init_code() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+  #endif
+}
+
+int main() {
+  init_code();
+
+  ll k, n, w;
+  cin >> k >> n >> w;
+
+  ll t = k * w * (w + 1) / 2;
+
+  cout << max(0ll, t - n);
+
+  return 0;
+}
