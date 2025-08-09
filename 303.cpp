e0e5f753bcc53b4ea7a50eaad7e33c7ae2d13af6
YolgeSanchez <imjorgesanchez@gmail.com>
2025-08-09 19:27:26 -0400

    range sum query 303 leetcode easy

---
+++ 303.cpp
@@ -0,0 +1,13 @@
+vector<int> prefixSum;
+
+NumArray(vector<int>& nums) {
+  int n = nums.size();
+  prefixSum.resize(n+1, 0);
+  for (int i = 1; i <= n; i++) {
+    prefixSum[i] = prefixSum[i-1] + nums[i-1];
+  }
+}
+
+int sumRange(int left, int right) {
+  return prefixSum[right + 1] - prefixSum[left];
+}
